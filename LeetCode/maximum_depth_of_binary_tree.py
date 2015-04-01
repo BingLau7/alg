5367dbaf643aac1813a5e71e768aa9512c8846fe
BingLau7 <binglau1819@gmail.com>
2015-04-01 19:41:53 +0800

    LeetCode 104

--- LeetCode/maximum_depth_of_binary_tree.py
+++ LeetCode/maximum_depth_of_binary_tree.py
@@ -12,4 +12,30 @@ class Solution:
     # @param root, a tree node
     # @return an integer
     def maxDepth(self, root):
-        pass
+        """
+            最大深度的最优子结构是以它的下一级子树为根也是最大深度
+            max_depth(root) = max(max_depth(root.left), max_depth(root.right)) + 1
+            当root为叶子结点则为0
+            有两个情况：
+                1. 没有左子树，则max_depth(root) = max(max_depth(root.right)) + 1
+                2. 没有右子树，则max_depth(root) = max(max_depth(root.left)) + 1
+        """
+        if root == None:
+            return 0
+        if root.left == None and root.right == None:
+            return 1
+        elif root.left == None:
+            return self.maxDepth(root.right) + 1
+        elif root.right == None:
+            return self.maxDepth(root.left) + 1
+        else:
+            max_depth = max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+            return max_depth
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(1)
+    root.right = TreeNode(4)
+    root.right.left = TreeNode(2)
+    root.right.left.right = TreeNode(2)
+    print Solution().maxDepth(root)
