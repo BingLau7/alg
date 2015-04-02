45d43f314ceb6f19b34ef2e73b057a5b9a1e726e
BingLau7 <binglau1819@gmail.com>
2015-04-02 17:28:06 +0800

    LeetCode 171

---
+++ LeetCode/excel_sheet_column_number.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def titleToNumber(self, s):
+        """
+            26进制与10进制的转换
+        """
+        num = 0
+        i = 0
+        s = s[::-1]
+        while i < len(s):
+            num += (ord(s[i]) - ord('A') + 1) * pow(26, i)
+            i += 1
+        return num
+
+if __name__ == '__main__':
+    print Solution().titleToNumber('AB')
+
