a96f3308d47909a90d274e1bf5f0ecfd60edad53
BingLau7 <binglau1819@gmail.com>
2015-04-04 00:20:50 +0800

    LeetCode 141

---
+++ LeetCode/linked_list_cycle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        """
+            速度差来得到
+        """
+        if not head:
+            return False
+        p = head
+        q = head
+
+        while p and q:
+            p = p.next
+            if not q.next:
+                return False
+            q = q.next.next
+            if p == q:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(3)
+    # head.next.next.next = head
+    print Solution().hasCycle(head)
+
