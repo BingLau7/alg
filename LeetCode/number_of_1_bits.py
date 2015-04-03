511d53b816535f55c747394452f510ee8f4bd06f
BingLau7 <binglau1819@gmail.com>
2015-04-03 20:45:07 +0800

    LeetCode 191

---
+++ LeetCode/number_of_1_bits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        num = list(bin(n)[2:])
+        return len([i for i in num if i != '0'])
+
+if __name__ == '__main__':
+    print Solution().hammingWeight(11)
