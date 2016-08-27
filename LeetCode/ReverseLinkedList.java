ebba6ecb6adb4d7666ed19e9a2017103f2ab44dd
BingLau7 <binglau1819@gmail.com>
2016-08-27 15:00:56 +0800

    反转链表指明反转位置，方法比较糙

--- LeetCode/ReverseLinkedList.java
+++ LeetCode/ReverseLinkedList.java
@@ -29,51 +29,58 @@ public class ReverseLinkedList {
         return q;
     }
 
-    public ListNode reverseList(ListNode head, int n, int m){
+    public ListNode reverseList(ListNode head, int m, int n){
+        if (head == null || head.next == null){
+            return head;
+        }
         ListNode _head = head;
         ListNode _tail = head;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < m - 1; i++) {
             _head = _head.next;
         }
-        for (int i = 0; i < m - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             _tail = _tail.next;
         }
 
-        ListNode head_t = _tail;
         ListNode tail_t = null;
         tail_t = _tail.next;
         _tail.next = null;
-        head_t = reverseList(_head);
-        _head.next = head_t;
-        ListNode _t = head_t;
-        // while(_t != null) {
-        //     System.out.println("------" + _t.val + ", " + _head.val);
-        //     _t = _t.next;
-        // }
-        // _t.next = tail_t;
-        // _t.next = head_t;
-        return _t;
-        // _t.next = _head;
-        // while (_head.next != null){
-        //     _head = _head.next;
-        // }
-        // _head.next = _tail;
-        // return head;
+        // 接头
+        ListNode head_t = head;
+        if (m > 2) {
+          for (int i = 0; i < m - 2; i++) {
+              head_t = head_t.next;
+          }
+        }
+        ListNode head_t_t = reverseList(_head);
+        if (m == 1){
+            head_t = head_t_t;
+            head = head_t_t;
+        } else {
+            head_t.next = head_t_t;
+        }
+        // 接尾
+        ListNode t = head_t;
+        while(t.next != null) {
+            t = t.next;
+        }
+        t.next = tail_t;
+        return head;
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        ListNode t = head.next;
-        t.next = new ListNode(3);
-        t = t.next;
-        t.next = new ListNode(4);
-        t = t.next;
-        t.next = new ListNode(5);
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(5);
+        // ListNode t = head.next;
+        // t.next = new ListNode(3);
+        // t = t.next;
+        // t.next = new ListNode(4);
+        // t = t.next;
+        // t.next = new ListNode(5);
 
         ReverseLinkedList r = new ReverseLinkedList();
 
-        ListNode tt = r.reverseList(head, 2, 4);
+        ListNode tt = r.reverseList(head, 1, 2);
         while(tt != null) {
             System.out.print(tt.val + " -> ");
             tt = tt.next;
