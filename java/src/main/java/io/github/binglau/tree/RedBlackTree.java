29c97e40418713d4fc895e2e687ab22c27fdf016
BingLau7 <binglau1819@gmail.com>
2016-11-03 14:55:37 +0800

    feat(RedBlackTree): 添加

--- java/src/main/java/io/github/binglau/tree/RedBlackTree.java
+++ java/src/main/java/io/github/binglau/tree/RedBlackTree.java
@@ -1,5 +1,10 @@
 package io.github.binglau.tree;
 
+
+import io.github.binglau.Queue;
+
+import java.util.NoSuchElementException;
+
 /**
  * 类RedBlackTree.java的实现描述
  *
@@ -12,5 +17,176 @@ package io.github.binglau.tree;
  *
  * @author bingjian.lbj 2016-10-31 下午11:24
  */
-public class RedBlackTree {
+public class RedBlackTree<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left, right;
+        int size;             // 结点数
+        boolean color;     // 由其父节点指向它的链接的颜色
+
+        public Node(Key key, Value val, int size, boolean color) {
+            this.color = color;
+            this.key = key;
+            this.size = size;
+            this.val = val;
+        }
+    }
+
+    private  boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.size;
+    }
+
+    private boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Returns the smallest key in the symbol table.
+     * @return the smallest key in the symbol table
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public Key min() {
+        if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
+        return min(root).key;
+    }
+
+    // the smallest key in subtree rooted at x; null if no such key
+    private Node min(Node x) {
+        // assert x != null;
+        if (x.left == null) return x;
+        else                return min(x.left);
+    }
+
+    /**
+     * Returns the largest key in the symbol table.
+     * @return the largest key in the symbol table
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public Key max() {
+        if (isEmpty()) throw new NoSuchElementException("called max() with empty symbol table");
+        return max(root).key;
+    }
+
+    // the largest key in the subtree rooted at x; null if no such key
+    private Node max(Node x) {
+        // assert x != null;
+        if (x.right == null) return x;
+        else                 return max(x.right);
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.size = h.size;
+        h.size = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.size = h.size;
+        h.size = 1 + size(h.left) + size(h.right);
+        return x;
+    }
+
+    // 转换一个节点的两个红色子结点的颜色
+    // 除了将子结点的颜色由红变黑之外,还要将父节点的颜色由黑转红
+    // h代表是父节点
+    private void flipColors(Node h){
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+    public Iterable<Key> keys() {
+        if (isEmpty()) return new Queue<Key>();
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        if (lo == null) throw new NullPointerException("first argument to keys() is null");
+        if (hi == null) throw new NullPointerException("second argument to keys() is null");
+
+        Queue<Key> queue = new Queue<Key>();
+        // if (isEmpty() || lo.compareTo(hi) > 0) return queue;
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    // add the keys between lo and hi in the subtree rooted at x
+    // to the queue
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws NullPointerException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new NullPointerException("argument to get() is null");
+        return get(root, key);
+    }
+
+    // value associated with the given key in subtree rooted at x; null if no such key
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else              return x.val;
+        }
+        return null;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) return new Node(key, val, 1, RED);
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, val);
+        else if (cmp > 0) h.right = put(h.right, key, val);
+        else h.val = val;
+
+        // 将任意含有红色右链接的3-结点(或临时的4-结点)向左旋转
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        // 将临时的4-结点中的两个连续红链接中的上层链接向右旋转
+        if (isRed(h.left) && !isRed(h.left.left)) h = rotateRight(h);
+        // 进行颜色转换并将红链接在树中向上传递
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.size = size(h.left) + size(h.right) - 1;
+        return h;
+    }
+
 }
