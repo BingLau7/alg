679d5e582720e24f9dc9f2f38d348ecaef61fe0d
BingLau7 <binglau1819@gmail.com>
2017-03-19 23:30:20 +0800

    Java:sort:quick sort

--- java/src/main/java/io/github/binglau/sort/QuickSort.java
+++ java/src/main/java/io/github/binglau/sort/QuickSort.java
@@ -10,27 +10,22 @@ public class QuickSort {
 
     private static void quickSort(int[] arr, int left, int right){
         if (left < right){
-            int pivot=partition(arr, left, right);
-            quickSort(arr, left, pivot - 1);
-            quickSort(arr, pivot + 1, right);
+            int pivotIndex=partition(arr, left, right);
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
         }
     }
     private static int partition(int[] arr, int left, int right) {
         int pivot = arr[left];
-        // m 为标记最近一个比主要大的数
-        // i 为前遍历数的索引
-        int m = left + 1, i;
-        int tmp;
-        for (i = left + 1; i <= right; i++) {
-            if (pivot > arr[i]) {
-                // m 与 i 互换
+        int tmp, m = left + 1;
+        for (int i = left + 1; i <= right; i++) {
+            if (pivot >= arr[i]) {
                 tmp = arr[i];
                 arr[i] = arr[m];
                 arr[m] = tmp;
-                m += 1;
+                m++;
             }
         }
-        // 主元与 m 前的元素互换
         tmp = arr[left];
         arr[left] = arr[m - 1];
         arr[m - 1] = tmp;
