6549f2cef11506e906f9bc5d94da502f3290e7b6
BingLau7 <binglau1819@gmail.com>
2017-11-09 00:21:05 +0800

    array: FirstMissingPositive: not pass

--- go-alg/test.go
+++ go-alg/test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var nums = [] int{10, 1, 2, 7, 6, 1, 5}
-	fmt.Println(alg.CombinationSum2(nums, 8))
-	//fmt.Println(utils.RemoveSliceInt(nums, len(nums)-1))
+	var nums = [] int{3, 4, -1, 1}
+	fmt.Println(alg.FirstMissingPositive(nums))
+	fmt.Println("---------------test--------------------")
 }
