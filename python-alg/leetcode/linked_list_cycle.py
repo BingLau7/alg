4c80cd25f764fb46c3e1f714709ba0902da90b05
liubingjian <binglau1819@gmail.com>
2017-12-29 14:08:14 +0800

    alg: swap pair python

--- python-alg/leetcode/linked_list_cycle.py
+++ python-alg/leetcode/linked_list_cycle.py
@@ -2,10 +2,7 @@
 # encoding: utf-8
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+from .list_node import ListNode
 
 class Solution:
     # @param head, a ListNode
